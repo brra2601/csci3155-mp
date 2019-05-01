211184aa306a69e65e9f96b1388afd3dfdf1f8a8
brandon <brandon@ubuntu>
2019-04-30 21:25:17 -0700

    added rest endpoint to get an animal sound based on injected animal class

--- src/main/java/com/miniproject/Api.java
+++ src/main/java/com/miniproject/Api.java
@@ -1,5 +1,6 @@
 package com.miniproject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api")
 public class Api {
 
+    @Autowired
+    private Animal animal;
+
     @GetMapping
     public ResponseEntity<Object> getHello() {
         return ResponseEntity.ok().body(new ApiResponse("Hello world!"));
+    }
+
+    @GetMapping(value = "/animal")
+    public ResponseEntity<Object> getAnimalNoise() {
+        String animalNoise = animal.makeNoise();
+        return ResponseEntity.ok().body(new ApiResponse(animalNoise));
     }
 
 }
